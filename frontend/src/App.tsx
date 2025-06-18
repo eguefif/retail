024ef3b8510b969eed6df15edcf27ed9d3a9a8cc
Emmanuel Guefif <eguefif@fastmail.com>
2025-06-18 07:53:45 -0400

    issue-6: extract component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,10 @@
 import './App.css'
-
-type ProductProps = {
-    product: Product,
-};
-
-function Product({ product }: ProductProps) {
-    return (
-        <div className="productCard">
-            <div className="title">{product.name}</div>
-            <div className="body">{product.description}</div>
-        </div>
-    )
-}
-
-type Product = {
-    name: string,
-    description: string
-}
+import ProductList from './ProductList.tsx'
 
 function App() {
-    const products = [
-        {
-            name: "Red Rooster",
-            description: "fsdfdsafds fsdf dsf dsf dsaf dsfsf sf sdf "
-        },
-        {
-            name: "Calla de choe",
-            description: "fsdfdsafds fsdf dsf dsf dsaf dsfsf sf sdf "
-        },
-    ];
-
-    const listProducts = products.map(product =>
-        <Product product={product} />
-    );
-
     return (
         <>
-            <div className="productList">
-                {listProducts}
-            </div>
+            <ProductList />
         </>
     )
 }
---
+++ frontend/src/ProductList.tsx
@@ -0,0 +1,43 @@
+
+type ProductProps = {
+    product: Product,
+};
+
+function Product({ product }: ProductProps) {
+    return (
+        <div className="productCard">
+            <div className="title">{product.name}</div>
+            <div className="body">{product.description}</div>
+        </div>
+    )
+}
+
+type Product = {
+    name: string,
+    description: string
+}
+
+export default function ProductList() {
+    const products = [
+        {
+            name: "Red Rooster",
+            description: "fsdfdsafds fsdf dsf dsf dsaf dsfsf sf sdf "
+        },
+        {
+            name: "Calla de choe",
+            description: "fsdfdsafds fsdf dsf dsf dsaf dsfsf sf sdf "
+        },
+    ];
+
+    const listProducts = products.map(product =>
+        <Product product={product} />
+    );
+
+    return (
+        <>
+            <div className="productList">
+                {listProducts}
+            </div>
+        </>
+    )
+}
